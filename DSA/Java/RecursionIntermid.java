ccf72bfa42390f7ce68e3b56a4448fb462805d38
vanshikacvjain@gmail.com <vanshikacvjain@gmail.com>
2026-06-09 22:53:50 +0530

    Updates

--- DSA/Java/RecursionIntermid.java
+++ DSA/Java/RecursionIntermid.java
@@ -29,18 +29,23 @@ public class RecursionIntermid {
 //    public static int first = -1;
 //    public static int last = -1;
 //
-//    public static void getIndices(String str, char el, int idx) {
-//        if (idx == str.length()) {
+//    public static void FindOcc(String s, int idx, char element){
+//        if (idx==s.length()){
+//            System.out.println("First Occurence: "+first);
+//            System.out.println("Last Occurence: "+last);
 //            return;
 //        }
-//        if (str.charAt(idx) == el) {
+//
+//        char curr=s.charAt(idx);
+//        if (curr == element) {
 //            if (first == -1) {
 //                first = idx;
+//                last = idx;
 //            } else {
 //                last = idx;
 //            }
 //        }
-//        getIndices(str, el, idx + 1);
+//        FindOcc(s,idx+1,element);
 //    }
 
     // array  sorted?
@@ -56,25 +61,24 @@ public class RecursionIntermid {
 //    }
 
     //move x
-//    public static String addX(int count) {
-//        String newStr = "x";
-//        for(int i=1;i<count; i++) {
-//            newStr += 'x';
-//        }
-//        return newStr;
-//    }
-//    public static String moveAllX(String str, int idx, int count) {
-//        if(idx == str.length()) {
-//            return addX(count);
-//        }
-//        if(str.charAt(idx) == 'x') {
-//
-//            return moveAllX(str, idx+1, count+1);
-//        } else {
-//            String nextStr = moveAllX(str, idx+1, count);
-//            return str.charAt(idx) + nextStr;
-//        }
-//    }
+    public static void MoveallX(String s,char x,int idx,int c,String news){
+        if (idx==s.length()){
+            for (int i=0;i<c;i++){
+                news+=x;
+            }
+            System.out.println("Updated String: "+news);
+            return;
+        }
+        char curr=s.charAt(idx);
+        if (curr==x){
+            c++;
+            MoveallX(s,x,idx+1,c,news);
+        }else {
+            news+=curr;
+            MoveallX(s,x,idx+1,c,news);
+        }
+    }
+
 
     // duplicates
 //    public static String removeDuplicates(String str, int idx, boolean present[]) {
@@ -102,7 +106,7 @@ public class RecursionIntermid {
 //    printSubseq(str, idx+1, res);
 //}
 
-//public static void printSubseq(String str, int idx, String res, HashSet<String> allSubseq) {
+//public static void printSubseq2(String str, int idx, String res, HashSet<String> allSubseq) {
 //    if(idx == str.length()) {
 //        if(allSubseq.contains(res)) {
 //            return;
@@ -118,29 +122,31 @@ public class RecursionIntermid {
 //    printSubseq(str, idx+1, res, allSubseq);
 //}
 
-public static String keypad[] = {".", "abc", "def", "ghi", "jkl", "mno", "pqrs",
-        "tu", "vwx", "yz"};
-    public static void printKeypadCombination(String number, int idx, String res) {
-        if(idx == number.length()) {
-            System.out.println(res);
-            return;
-        }
-
-        for(int i=0; i<keypad[number.charAt(idx)-'0'].length(); i++) {
-            char curr = keypad[number.charAt(idx)-'0'].charAt(i);
-            printKeypadCombination(number, idx+1, res+curr);
-        }
-    }
+//public static String keypad[] = {".", "abc", "def", "ghi", "jkl", "mno", "pqrs",
+//        "tu", "vwx", "yz"};
+//    public static void printKeypadCombination(String number, int idx, String res) {
+//        if(idx == number.length()) {
+//            System.out.println(res);
+//            return;
+//        }
+//
+//        for(int i=0; i<keypad[number.charAt(idx)-'0'].length(); i++) {
+//            char curr = keypad[number.charAt(idx)-'0'].charAt(i);
+//            printKeypadCombination(number, idx+1, res+curr);
+//        }
+//    }
 
 
 
     public static void main (String args[]){
-//        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 //      System.out.println("Enter value of n: ");
 //      int n = sc.nextInt();
 
 //      System.out.println("enter string: ");
 //      String str = sc.nextLine();
+
+
 //        System.out.println("enter size of array:");
 //        int size = sc.nextInt();
 //        int arr[] = new int[size];
@@ -150,18 +156,27 @@ public static String keypad[] = {".", "abc", "def", "ghi", "jkl", "mno", "pqrs",
 //        }
 //        System.out.println(isSorted(arr, 0));
 
-//        int count = 0;
-//        String newStr = moveAllX(str, 0, count);
-//        System.out.println(newStr);
+
+        System.out.println("Enter the string: ");
+        String s = sc.nextLine();
+        System.out.println("Enter the character to move at last: ");
+        char x = sc.next().charAt(0);
+        MoveallX(s,x,0,0," ");
+
+
 //      towerOfHanoi(n, "S", "H", "D");
+
 
 //      String reversed = revString(str);
 //      System.out.println(reversed);
 
-//        char el = 'a';
-//        getIndices(str, el, 0);
-//        System.out.println("First occurence : " + first);
-//        System.out.println("Last occurence : " + last);
+
+//        Scanner vc = new Scanner(System.in);
+//        System.out.println("Enter the string: ");
+//        String s =vc.nextLine();
+//        System.out.println("Enter the a character u want to search: ");
+//        char c=vc.next().charAt(0);
+//        FindOcc(s,0,c);
 
 //        boolean present[] = new boolean[str.length()];
 //        System.out.println(removeDuplicates(str, 0, present));
@@ -174,10 +189,10 @@ public static String keypad[] = {".", "abc", "def", "ghi", "jkl", "mno", "pqrs",
 //        String str1 = "abc";
 //        String str2 = "aaa";
 //        HashSet<String> allSubseq = new HashSet<>();
-//        printSubseq(str2, 0, "", allSubseq);
+//        printSubseq2(str2, 0, "", allSubseq);
 
-        String number = "23";
-        printKeypadCombination(number, 0, "");
+//        String number = "23";
+//        printKeypadCombination(number, 0, "");
 
     }
 }
